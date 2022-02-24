bfa630971bf392a5c6961def3affa0f4b094cd72
MrKillerBird <danny.vankampen05@gmail.com>
2022-02-24 11:08:46 +0100

    template functie toegevoegd

--- Math Mate/js/berekeningen.js
+++ Math Mate/js/berekeningen.js
@@ -1,3 +1,21 @@
+//gebruik dit als template voor de berekeningen
+/*
+function berekening(){ //verander de naam van de functie naar de naam van de berekening
+    let input = document.getElementById("berekening-input-1").value; //verander id naar de juiste input veld
+    let output = document.getElementById("berekening-output") //verander id naar de juiste output veld
+    output.innerHTML = "";
+    //hieronder je berekening
+
+
+
+}
+document.getElementById("berekening-button").addEventListener("click", fibonacci) //verander naar de juiste button id en naam van de functie
+*/
+
+
+
+
+
 function fibonacci(){
     let input = document.getElementById("fibonacci-input-1").value;
     let output = document.getElementById("fibonacci-output")
