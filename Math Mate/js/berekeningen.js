a7ed5a138173fdb56dae3b69f40b05b07bdcf005
MrKillerBird <danny.vankampen05@gmail.com>
2022-02-24 11:51:52 +0100

    missende ; toegevoegd

--- Math Mate/js/berekeningen.js
+++ Math Mate/js/berekeningen.js
@@ -3,23 +3,21 @@
 function berekening(){ //verander de naam van de functie naar de naam van de berekening
     let input1 = document.getElementById("berekening-input-1").value; //verander id naar de juiste input veld
     let input2 = document.getElementById("berekening-input-2").value; //gebruik dit als er een tweede input is
-    let output = document.getElementById("berekening-output") //verander id naar de juiste output veld
+    let output = document.getElementById("berekening-output"); //verander id naar de juiste output veld
     output.innerHTML = "";
     //hieronder je berekening
 
 
 
 }
-document.getElementById("berekening-button").addEventListener("click", berekening) //verander naar de juiste button id en naam van de functie
+document.getElementById("berekening-button").addEventListener("click", berekening); //verander naar de juiste button id en naam van de functie
 */
 
 
 
-
-
-function fibonacci(){
+function fibonacci(){ //Danny
     let input1 = document.getElementById("fibonacci-input-1").value;
-    let output = document.getElementById("fibonacci-output")
+    let output = document.getElementById("fibonacci-output");
     output.innerHTML = "";
 
     let b = 0, temp;
@@ -30,4 +28,21 @@ function fibonacci(){
         b = temp;
     }
 }
-document.getElementById("fibonacci-button").addEventListener("click", fibonacci)
+document.getElementById("fibonacci-button").addEventListener("click", fibonacci);
+
+
+function factorizer(){ //Danny
+    let input1 = document.getElementById("factorizer-input-1").value;
+    let output = document.getElementById("factorizer-output");
+    output.innerHTML = "";
+    
+
+
+
+}
+document.getElementById("factorizer-button").addEventListener("click", factorizer);
+
+
+
+
+
