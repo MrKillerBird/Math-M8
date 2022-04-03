2d9e9809f7fd3e8e7723b70c53c500fee0634600
MrKillerBird <danny.vankampen05@gmail.com>
2022-04-03 11:27:56 +0200

    morsenizer af

--- Math Mate/js/berekeningen.js
+++ Math Mate/js/berekeningen.js
@@ -580,93 +580,187 @@ function morsenizer() { //Danny
             woorden.splice(i, 1);
         }
     }
-    output.innerHTML += woorden + "<br>";
+
     function morseEncode(letter) {
         switch (letter) {
             case "A": case "a":
                 return ".-";
-                
+
             case "B": case "b":
                 return "-..";
-                
+
             case "C": case "c":
                 return "-.-.";
-                
+
             case "D": case "d":
                 return "-..";
-                
+
             case "E": case "e":
                 return ".";
-                
+
             case "F": case "f":
                 return "..-.";
-                
+
             case "G": case "g":
                 return "--.";
-                
+
             case "H": case "h":
                 return "....";
-                
+
             case "I": case "i":
                 return "..";
-                
+
             case "J": case "j":
                 return ".---";
-                
+
             case "K": case "k":
                 return "-.-";
-                
+
             case "L": case "l":
                 return ".-..";
-                
+
             case "M": case "m":
                 return "--";
-                
+
             case "N": case "n":
                 return "-.";
-                
+
             case "O": case "o":
                 return "---";
-                
+
             case "P": case "p":
                 return ".--.";
-                
+
             case "Q": case "q":
                 return "--.-";
-                
+
             case "R": case "r":
                 return ".-.";
-                
+
             case "S": case "s":
                 return "...";
-                
+
             case "T": case "t":
                 return "-";
-                
+
             case "U": case "u":
                 return "..-";
-                
+
             case "V": case "v":
                 return "...-";
-                
+
             case "W": case "w":
                 return ".--";
-                
+
             case "X": case "x":
                 return "-..-";
-                
+
             case "Y": case "y":
                 return "-.--";
-                
+
             case "Z": case "z":
                 return "--..";
+
+
+            case "0":
+                return "-----";
+
+            case "1":
+                return ".----";
+
+            case "2":
+                return "..---";
+
+            case "3":
+                return "...--";
+
+            case "4":
+                return "....-";
+
+            case "5":
+                return ".....";
+
+            case "6":
+                return "-....";
+
+            case "7":
+                return "--...";
+
+            case "8":
+                return "---..";
+
+            case "9":
+                return "----.";
+
+            case ".":
+                return ".-.-.-";
+
+            case ",":
+                return "--..--";
+
+            case "?":
+                return "..--..";
+
+            case "'":
+                return ".----.";
+
+            case "!":
+                return "-.-.--";
+
+            case "/":
+                return "-..-.";
+
+            case "(":
+                return "-.--.";
+
+            case ")":
+                return "-.--.-";
+
+            case "&":
+                return ".-...";
+
+            case ":":
+                return "---...";
+
+            case ";":
+                return "-.-.-.";
+
+            case "=":
+                return "-...-";
+
+            case "+":
+                return ".-.-.";
+
+            case "-":
+                return "-....-";
+
+            case "_":
+                return "..--.-";
+
+            case '"':
+                return ".-..-.";
+
+            case "$":
+                return "...-..-";
+
+            case "@":
+                return ".--.-.";
+
+
+            default:
+                return "?";
         }
     }
 
+    for (let i = 0; i < woorden.length; i++) {
+        let letters = woorden[i].split("");
+        for (let i = 0; i < letters.length; i++) {
+            letters[i] = morseEncode(letters[i]);
+        }
+        woorden[i] = letters.join(" ");
+    }
 
-
-    
-    //output.innerHTML = woorden;
+    output.innerHTML = woorden.join(" / ");
 }
 if (document.getElementById("morsenizer-button")) {
     document.getElementById("morsenizer-button").addEventListener("click", morsenizer);
